741a2f816e8b659a129084b0fe2dbec19941549e
Medina <kulmagambetovamedina120@gmail.com>
2026-05-01 21:57:55 +0500

    fix tests import

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from utils import add, subtract, multiply
+
 
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_subtract():
     assert subtract(5, 2) == 3
+
 
 def test_multiply():
     assert multiply(4, 3) == 12
